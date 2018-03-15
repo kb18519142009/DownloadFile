9cd5197b9ddf2d078c11b117bd6108c24a485a13
kb18519142009 <847584597@qq.com>
2018-03-15 16:03:19 +0800

    规范变量命名等

--- app/src/main/java/com/example/downloadfile/activity/MainActivity.java
+++ app/src/main/java/com/example/downloadfile/activity/MainActivity.java
@@ -4,13 +4,19 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.downloadfile.R;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = "MainActivity";
 
     private Button mDownloadPicture;
     private Button mDownloadVideo;

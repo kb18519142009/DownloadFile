3eda693c971db54625b8c939012b12d3e40cd2b7
kb18519142009 <847584597@qq.com>
2018-05-17 17:43:25 +0800

    修改单例实现方式

--- app/src/main/java/com/example/downloadfile/activity/DownloadVideoActivity.java
+++ app/src/main/java/com/example/downloadfile/activity/DownloadVideoActivity.java
@@ -121,6 +121,7 @@ public class DownloadVideoActivity extends AppCompatActivity implements View.OnC
                     .send();
         } else {
             downloadVideo(); //处理具体下载过程
+
         }
     }
 

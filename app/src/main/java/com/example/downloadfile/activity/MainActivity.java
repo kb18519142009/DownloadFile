18f7616c63657d341442e8f40679cf7b79e0c917
kb18519142009 <847584597@qq.com>
2018-05-29 10:06:01 +0800

    整理

--- app/src/main/java/com/example/downloadfile/activity/MainActivity.java
+++ app/src/main/java/com/example/downloadfile/activity/MainActivity.java
@@ -4,9 +4,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
@@ -14,10 +13,6 @@ import android.widget.TextView;
 
 import com.example.downloadfile.R;
 import com.example.downloadfile.util.SystemUtil;
-
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 

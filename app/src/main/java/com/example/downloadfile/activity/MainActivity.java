eaf962a04b22a4a99096fbbcac056593dd6f6e95
kb18519142009 <847584597@qq.com>
2018-06-11 10:53:33 +0800

    整理

--- app/src/main/java/com/example/downloadfile/activity/MainActivity.java
+++ app/src/main/java/com/example/downloadfile/activity/MainActivity.java
@@ -27,6 +27,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             SystemUtil.setLightStatusBar(this, Color.WHITE);
         }

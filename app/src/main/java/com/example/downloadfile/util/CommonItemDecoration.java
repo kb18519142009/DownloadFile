b062a2e6b2bf842f4875ebf6b77f26df9fb6473c
kb18519142009 <847584597@qq.com>
2018-06-20 10:08:24 +0800

    添加注释等

--- app/src/main/java/com/example/downloadfile/util/CommonItemDecoration.java
+++ app/src/main/java/com/example/downloadfile/util/CommonItemDecoration.java
@@ -7,14 +7,18 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.View;
 
+/**
+ * Description：RecyclerView的万能间距
+ * Created by kang on 2018/3/9.
+ */
 public class CommonItemDecoration extends RecyclerView.ItemDecoration {
 
-    private int horizontalSpace;
-    private int verticalSpace;
-    private int leftMargin;
-    private int topMargin;
-    private int rightMargin;
-    private int bottomMargin;
+    private int horizontalSpace; // 水平方向间距
+    private int verticalSpace; // 垂直方向间距
+    private int leftMargin; // 与左边的间距
+    private int topMargin; // 与顶部的间距
+    private int rightMargin; // 与右边的间距
+    private int bottomMargin; // 与底部的间距
 
     public CommonItemDecoration(int horizontalSpace, int verticalSpace) {
         this(horizontalSpace, verticalSpace, 0, 0, 0, 0);
@@ -36,49 +40,73 @@ public class CommonItemDecoration extends RecyclerView.ItemDecoration {
     @Override
     public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
         super.getItemOffsets(outRect, view, parent, state);
+        // 得到当前Item在RecyclerView中的位置,从0开始
         int position = parent.getChildAdapterPosition(view);
+        // 得到RecyclerView中Item的总个数
         int count = parent.getAdapter().getItemCount();
-        if (parent.getLayoutManager() instanceof GridLayoutManager) {
+
+        if (parent.getLayoutManager() instanceof GridLayoutManager) { // 网格布局
             GridLayoutManager gridLayoutManager = (GridLayoutManager) parent.getLayoutManager();
+            // 得到网格布局的列数
             int spanCount = gridLayoutManager.getSpanCount();
-            if (LinearLayoutManager.VERTICAL == gridLayoutManager.getOrientation()) {
-                if (spanCount == 1) {
+            // 判断该网格布局是水平还是垂直
+            if (LinearLayoutManager.VERTICAL == gridLayoutManager.getOrientation()) { // 垂直
+                if (spanCount == 1) { // 列数为1
                     verticalColumnOne(outRect, position, count);
-                } else {
+                } else { // 列数大于1
                     verticalColumnMulti(outRect, position, count, spanCount);
                 }
-            } else if (LinearLayoutManager.HORIZONTAL == gridLayoutManager.getOrientation()) {
-                if (spanCount == 1) {
+            } else if (LinearLayoutManager.HORIZONTAL == gridLayoutManager.getOrientation()) { // 水平
+                if (spanCount == 1) { // 行数为1
                     horizontalColumnOne(outRect, position, count);
-                } else {
+                } else { // 行数大于1
                     horizontalColumnMulti(outRect, position, count, spanCount);
                 }
             }
-        } else if (parent.getLayoutManager() instanceof LinearLayoutManager) {
+        } else if (parent.getLayoutManager() instanceof LinearLayoutManager) { // 线性布局
             LinearLayoutManager layoutManager = (LinearLayoutManager) parent.getLayoutManager();
-            if (LinearLayoutManager.VERTICAL == layoutManager.getOrientation()) {
+            if (LinearLayoutManager.VERTICAL == layoutManager.getOrientation()) { // 垂直
                 verticalColumnOne(outRect, position, count);
-            } else if (LinearLayoutManager.HORIZONTAL == layoutManager.getOrientation()) {
+            } else if (LinearLayoutManager.HORIZONTAL == layoutManager.getOrientation()) { // 水平
                 horizontalColumnOne(outRect, position, count);
             }
-        } else if (parent.getLayoutManager() instanceof StaggeredGridLayoutManager) {
+        } else if (parent.getLayoutManager() instanceof StaggeredGridLayoutManager) { // 流布局
             //TODO
         }
     }
 
+    /**
+     * 列表垂直且列数为1
+     *
+     * @param outRect  包括左上右下四个参数，分别控制view左上右下的margin
+     * @param position 当前view所处位置
+     * @param count    RecyclerView中Item的总个数
+     */
     private void verticalColumnOne(Rect outRect, int position, int count) {
-        if (position == 0) {
+        if (position == 0) { // 位置为0时(即第一个Item)，不设置底部间距
             outRect.set(leftMargin, topMargin, rightMargin, 0);
-        } else if (position == count - 1) {
+        } else if (position == count - 1) { // 最后一个Item
             outRect.set(leftMargin, verticalSpace, rightMargin, bottomMargin);
-        } else {
+        } else { // 中间的Item，不设置底部间距
             outRect.set(leftMargin, verticalSpace, rightMargin, 0);
         }
     }
 
+    /**
+     * 列表垂直且列数大于1
+     *
+     * @param outRect   包括左上右下四个参数，分别控制view左上右下的margin
+     * @param position  当前view所处位置
+     * @param count     RecyclerView中Item的总个数
+     * @param spanCount 布局的列数
+     */
     private void verticalColumnMulti(Rect outRect, int position, int count, int spanCount) {
+        // 通过计算得出总行数
         int totalRow = count / spanCount + ((count % spanCount) == 0 ? 0 : 1);
+        // 计算得出当前view所在的行
         int row = position / spanCount;
+        // 通过对position加1对spanCount取余得到column
+        // 保证column等于1为第一列，等于0为最后一个，其它值为中间列
         int column = (position + 1) % spanCount;
         if (column == 1) {
             outRect.set(leftMargin,
@@ -98,19 +126,38 @@ public class CommonItemDecoration extends RecyclerView.ItemDecoration {
         }
     }
 
+    /**
+     * 列表水平且行数为1
+     *
+     * @param outRect  包括左上右下四个参数，分别控制view左上右下的margin
+     * @param position 当前view所处位置
+     * @param count    RecyclerView中Item的总个数
+     */
     private void horizontalColumnOne(Rect outRect, int position, int count) {
-        if (position == 0) {
+        if (position == 0) { // 位置为0时(即第一个Item)
             outRect.set(leftMargin, topMargin, horizontalSpace / 2, bottomMargin);
-        } else if (position == count - 1) {
+        } else if (position == count - 1) { // 最后一个Item
             outRect.set(horizontalSpace / 2, topMargin, rightMargin, bottomMargin);
-        } else {
+        } else { // 中间的Item
             outRect.set(horizontalSpace / 2, topMargin, horizontalSpace / 2, bottomMargin);
         }
     }
 
+    /**
+     * 列表水平且行数大于1
+     *
+     * @param outRect   包括左上右下四个参数，分别控制view左上右下的margin
+     * @param position  当前view所处位置
+     * @param count     RecyclerView中Item的总个数
+     * @param spanCount 布局的行数
+     */
     private void horizontalColumnMulti(Rect outRect, int position, int count, int spanCount) {
+        // 通过计算得出总列数
         int totalColumn = count / spanCount + ((count % spanCount) == 0 ? 0 : 1);
+        // 计算得出当前view所在的列
         int column = position / spanCount;
+        // 通过对position加1对spanCount取余得到row
+        // 保证row等于1为第一行，等于0为最后一个，其它值为中间行
         int row = (position + 1) % spanCount;
         if (row == 1) {
             outRect.set(column == 0 ? leftMargin : horizontalSpace / 2,

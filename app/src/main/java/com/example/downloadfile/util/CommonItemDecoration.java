b7b4f65fae1649e7f5f66932daf8483e4a462d8e
kb18519142009 <847584597@qq.com>
2018-08-29 18:54:38 +0800

    整理

--- app/src/main/java/com/example/downloadfile/util/CommonItemDecoration.java
+++ app/src/main/java/com/example/downloadfile/util/CommonItemDecoration.java
@@ -19,6 +19,7 @@ public class CommonItemDecoration extends RecyclerView.ItemDecoration {
     private int leftMargin; // 每个item与左边的间距
     private int topMargin; // 每个item与顶部的间距
     private int rightMargin; // 每个item与右边的间距
+
     private int bottomMargin; // 每个item与底部的间距
 
     public CommonItemDecoration(int horizontalSpace, int verticalSpace) {

024c473d8e537cdbb1d4ac02a88df50e51397633
kb18519142009 <847584597@qq.com>
2018-09-29 14:53:08 +0800

    整理

--- app/src/main/java/com/example/downloadfile/activity/DownloadVideoActivity.java
+++ app/src/main/java/com/example/downloadfile/activity/DownloadVideoActivity.java
@@ -59,6 +59,7 @@ public class DownloadVideoActivity extends AppCompatActivity implements View.OnC
     //不知道还能不能用
     private static final String PLAY_VIDEO_URL =
             "http://c1.daishumovie.com/7d2b85ce0799dacb96c3949e25d74678/5aa40100/video/client/2018/1/9/3F8F87CC040147F28AC97A4733491A8A_640x360_200_48_24.mp4";
+
     private static final DefaultBandwidthMeter BANDWIDTH_METER = new DefaultBandwidthMeter();
 
     private FrameLayout mBackLayout; //返回按钮

21caae94d9544401c75ae391b4795dc015b1500e
kb18519142009 <847584597@qq.com>
2018-12-10 10:29:29 +0800

    整理

--- app/src/main/java/com/example/downloadfile/activity/MainActivity.java
+++ app/src/main/java/com/example/downloadfile/activity/MainActivity.java
@@ -18,7 +18,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private static final String TAG = "MainActivity";
     private Button mDownloadPicture; //下载图片按钮
     private Button mDownloadVideo; //下载视频按钮
-
     private Context mContext;
 
     @Override

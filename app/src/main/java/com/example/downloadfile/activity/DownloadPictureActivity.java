8f3652bc5cc7fec583581694a44b549a1def2185
kb18519142009 <847584597@qq.com>
2019-03-08 14:28:12 +0800

    整理

--- app/src/main/java/com/example/downloadfile/activity/DownloadPictureActivity.java
+++ app/src/main/java/com/example/downloadfile/activity/DownloadPictureActivity.java
@@ -37,6 +37,7 @@ public class DownloadPictureActivity extends AppCompatActivity implements View.O
     private ImageView mPicture;
     private FrameLayout mCircleProgressLayout;
     private KbWithWordsCircleProgressBar mCircleProgress;
+
     private Context mContext;
 
     @Override
